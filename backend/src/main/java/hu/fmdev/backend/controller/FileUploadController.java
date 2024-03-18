a34eec87a78e4395f06484e284e43478608adce8
marton.fabian <nagyonmarci@gmail.com>
2024-03-18 16:17:50 +0100

    minimal refactoring

--- backend/src/main/java/hu/fmdev/backend/controller/FileUploadController.java
+++ backend/src/main/java/hu/fmdev/backend/controller/FileUploadController.java
@@ -4,15 +4,15 @@ import hu.fmdev.backend.service.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.nio.file.Path;
 
-@Controller
+@RestController
 public class FileUploadController {
 
     @Autowired
--- backend/src/main/java/hu/fmdev/backend/controller/PdfFormFillerController.java
+++ backend/src/main/java/hu/fmdev/backend/controller/PdfFormFillerController.java
@@ -6,13 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/pdf")
 public class PdfFormFillerController {
 
