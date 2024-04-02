707feaea345eb23674d6de00b74e5e0ba94cd68b
marton.fabian <nagyonmarci@gmail.com>
2024-04-02 10:13:56 +0200

    mongodb

--- backend/src/main/java/hu/fmdev/backend/repository/nodetypes/CommitmentClaimRepository.java
+++ backend/src/main/java/hu/fmdev/backend/repository/nodetypes/CommitmentClaimRepository.java
@@ -1,9 +1,9 @@
 package hu.fmdev.backend.repository.nodetypes;
 
 import hu.fmdev.backend.domain.nodetypes.CommitmentClaim;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommitmentClaimRepository extends JpaRepository<CommitmentClaim, Long> {
+public interface CommitmentClaimRepository extends MongoRepository<CommitmentClaim, String> {
 }
