986299f28c8fe785aa796668326067f52208e482
marton.fabian <nagyonmarci@gmail.com>
2024-04-02 10:12:09 +0200

    mongodb

--- backend/src/main/java/hu/fmdev/backend/repository/AuthorityRepository.java
+++ backend/src/main/java/hu/fmdev/backend/repository/AuthorityRepository.java
@@ -1,10 +1,9 @@
 package hu.fmdev.backend.repository;
 
 import hu.fmdev.backend.domain.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+public interface AuthorityRepository extends MongoRepository<Authority, String> {
 }
-
--- backend/src/main/java/hu/fmdev/backend/repository/UserRepository.java
+++ backend/src/main/java/hu/fmdev/backend/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package hu.fmdev.backend.repository;
 
 import hu.fmdev.backend.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, String> {
     User findByUsername(String username);
 }
